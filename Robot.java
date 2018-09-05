5c679db0c87b65064ba7dcabb81522ead9039d13
lauramrice <42754734+lauramrice@users.noreply.github.com>
2018-09-05 13:55:52 -0500

    /src

---
+++ Robot.java
@@ -0,0 +1,8 @@
+package threesolid;
+
+public class Robot implements IWorkable {
+
+	public void work() {
+		//.....only working
+	}
+}
---
+++ Worker.java
@@ -0,0 +1,12 @@
+package threesolid;
+
+public class Worker implements IWorkable, IFeedable {
+
+	public void work() {
+		// ....working
+	}
+
+	public void eat() {
+		//.... eating in launch break
+	}
+}
