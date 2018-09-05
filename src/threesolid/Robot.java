9980034553a97f82c30cf25e1b5d43c84e403922
lauramrice <42754734+lauramrice@users.noreply.github.com>
2018-09-05 17:43:08 -0500

    Add files via upload

--- src/threesolid/Robot.java
+++ src/threesolid/Robot.java
@@ -1,7 +1,16 @@
+// Laura Rice
+// We have separated the Robot class so that it has its own java file.  
+// As we extend this class, the functions of Robot will differ from those of Worker and Superworker, so 
+// creating Robot as a separate class will allow a user to add more functionality to the code without affecting
+// Worker or Superworker.  This applies the Single Responsibility principal.  
+// We have also applied the Open Close principal by separating the class.  We can add on to the existing code 
+// without modifying it.  
+
 package threesolid;
 
 public class Robot implements IWorkable {
+
 	public void work() {
-		// .....only working
+		//.....only working
 	}
 }
--- src/threesolid/Worker.java
+++ src/threesolid/Worker.java
@@ -1,11 +1,21 @@
+// Laura Rice
+// We have separated the Worker class so that it has its own java file.  
+// As we extend this class, the functions of Worker will differ from those of Robot and Superworker, so 
+// creating Worker as a separate class will allow a user to add more functionality to the code without affecting
+// Robot or Superworker.  This applies the Single Responsibility principal.  
+// We have also applied the Open Close principal by separating the class.  We can add on to the existing code 
+// without modifying it.  
+
+
 package threesolid;
 
 public class Worker implements IWorkable, IFeedable {
+
 	public void work() {
 		// ....working
 	}
 
 	public void eat() {
-		// ....eating in launch break
+		//.... eating in launch break
 	}
 }
