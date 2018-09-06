67a0f06f657f6bae786e48d0267499435cfb56fe
jazminec <42793645+jazminec@users.noreply.github.com>
2018-09-05 22:40:17 -0500

    Added description for Manager

--- src/threesolid/Manager.java
+++ src/threesolid/Manager.java
@@ -1,3 +1,13 @@
+// Jazmine Castillo
+//
+// This class applies to the manager, who will manage the different types of workers.
+// Creating a separate file ensures only code associated with the manager is placed in this file.
+// This complies with the Open Close Principle because new code can be added easily while still
+// minimizing changes to current code.
+// This file implements the Single Responsibility Principle because the activities of the manager
+// differ from the workers' activities, therefore changes to workers' code will require minimal changes
+// to this file.
+
 package threesolid;
 
 class Manager {
