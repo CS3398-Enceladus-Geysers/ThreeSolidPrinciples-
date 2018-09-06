090e00db24dfde243c2b4e03492546ad1238497e
Maria F Jaramillo <32345851+mfj7@users.noreply.github.com>
2018-09-05 19:03:21 -0500

    Add files via upload

---
+++ bin/threesolid/SuperWorker.java
@@ -0,0 +1,14 @@
+// Maria F Jaramillo
+// 
+package threesolid;
+
+public class SuperWorker implements IWorkable, IFeedable {
+
+	public void work() {
+		// ....working
+	}
+
+	public void eat() {
+		//.... eating in launch break
+	}
+}
