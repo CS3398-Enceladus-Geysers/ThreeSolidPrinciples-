7c727e47a664ee7a61f6a9e057770fab11e2dc27
Peter Wesley Hutcheson <hutcheson8@gmail.com>
2018-09-05 19:24:23 -0500

    Added Code to Main.java

--- src/threesolid/Main.java
+++ src/threesolid/Main.java
@@ -2,5 +2,16 @@ package threesolid;
 
 public class Main {
 	public static void main(String[] args) {
+		try {
+			System.out.format("Starting ... \n");
+		} catch (Exception main_except) {
+			main_except.printStackTrace();
+		}
+		try {
+			System.out.format("Stopping ... \n");
+		} catch (Exception main_except) {
+			main_except.printStackTrace();
+		}
+		System.exit(0);
 	}
 }
