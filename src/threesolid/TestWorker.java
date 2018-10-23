7ae96fc79294bb94ac2acbad82c3c9461b8f3cac
KYork <42783403+kjy9@users.noreply.github.com>
2018-10-23 17:39:55 -0500

    Create TestWorker.java

---
+++ src/threesolid/TestWorker.java
@@ -0,0 +1,13 @@
+package <threesoild>;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+public class TestWorker {
+ private Worker w = new Worker();
+ @Test
+ @DisplayName("Work Method Test")
+ public void testWorkMethod()
+ {
+ assertEquals(w.work(),"Working...\n",”Mismatch between test text and method text”);
+ }
+}
