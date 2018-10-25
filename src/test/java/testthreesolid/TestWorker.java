9cbbf665283ae3b8578dc14f018518193a9dc009
Maria F Jaramillo <32345851+mfj7@users.noreply.github.com>
2018-10-25 14:55:21 -0500

    update TestWorker

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestWorker {
- private Worker w = new Worker();
- @Test
- @DisplayName("Work Method Test")
- public void testWorkMethod()
- {
-    assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
- }
+	private Worker w = new Worker();
+	@Test
+	@DisplayName("Work Method Test")
+	public void testWorkMethod()
+	{
+		assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
+	}
 };
