3367dad5020f6d7eddc15f201d0741eadcf8c3d1
KYork <42783403+kjy9@users.noreply.github.com>
2018-10-25 17:49:49 -0500

    Add files via upload

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -7,19 +7,8 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
  private Worker w = new Worker();
  @Test
- @DisplayName("Work Method Test")
- public void testWorkMethod()
- {
-    assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
- }
- @Test
  @DisplayName("newTest<kjy>WorkerPass")
  public void WorkerPass() {
  	 assertEquals(w.work(),"Working...\n", "Correct."); //Kristof York
- }
- @Test
- @DisplayName("newTest<kjy>WorkerFail")
- public void WorkerFail() {
- 	assertEquals(w.work(),"Maybe working.. ;) ", "Mismatch between test text and method text."); //Kristof York 
  }
 };
